cb1dad5115986e081f236fa586e89146f13e19ff
Bogdan Petrut <bogdanpetrut.319@gmail.com>
2024-04-17 02:30:36 +0200

    Refactor variable names and update display in script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let display = document.querySelector("#display");
-let buttons = document.querySelectorAll(".number, .operator");
-let clearButton = document.querySelector("#clear");
-let equalsButton = document.querySelector("#equals");
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll(".number, .operator");
+const clear_button = document.querySelector("#clear");
+const equals_button = document.querySelector("#equals");
 
-let expression = {};
-let displayValue = "";
+let expression = [];
+let display_value = "0";
+let last_number = "";
 
 
 function add(a, b) { return a + b; }
@@ -12,7 +13,7 @@ function subtract(a, b) { return a - b; }
 function multiply(a, b) { return a * b; }
 function divide(a, b) {
     if (b === 0) {
-        return "You can't divide by 0!";
+        return "ERR_DIV_ZERO";
     }
     return a / b;
 }
@@ -32,11 +33,53 @@ function operate(operator, a, b) {
     }
 }
 function updateDisplay() {
-    display.textContent = displayValue;
+    display.textContent = display_value;
 }
 function clear() {
-    displayValue = "0";
-    expression = {};
+    expression = [];
+    display_value = "0";
+    last_number = "";
     updateDisplay();
 }
 
+function calculate() {
+    if (expression.length === 3) {
+        let result = operate(expression[1], expression[0], expression[2]);
+        
+        if (result === "ERR_DIV_ZERO") {
+            display_value = "ERROR: you can't divide by zero!";
+            expression = [];
+            updateDisplay();
+            return;
+        }
+        display_value = Math.round(result * 100) / 100;
+        expression = [result];
+        last_number = "";
+        updateDisplay();
+    }
+}
+
+
+// ### Event Listeners ###
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (button.classList.contains("operator")) {
+            expression.push(last_number); // fixme: if last_number is empty, it will push an empty string
+            if (expression.length === 3) calculate(); // calculate if 3 elements in expression
+            if (expression.length === 2) expression.pop(); // replace operator
+            expression.push(button.textContent);
+        }
+        else 
+        if (button.classList.contains("number")) {
+            if ( display_value === "0" ) display_value = "";
+            last_number += button.textContent;
+        }
+        display_value += button.textContent;
+        updateDisplay();
+    });
+});
+clear_button.addEventListener("click", clear);
+equals_button.addEventListener("click", () => {
+    expression.push(last_number); //fixme: if last_number is empty, it will push an empty string
+    calculate();
+});
