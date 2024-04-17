003bf8aa666df8362b6c8f0e58d4b66c45a8e48e
SWasn003 <ahsan19saeed@gmail.com>
2024-04-17 12:40:30 +0200

    fixed displayed number

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ let display_value = "0";
 let last_number = "";
 
 
-function add(a, b) { return a + b; }
+function add(a, b) { 
+    return a + b; }
 function subtract(a, b) { return a - b; }
 function multiply(a, b) { return a * b; }
 function divide(a, b) {
